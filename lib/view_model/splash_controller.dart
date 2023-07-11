1d0c2078f36389e571d57422ddd5790ad5ef3b0d
Abiidahiya <dahiya.abhi@live.com>
2023-07-11 15:50:32 +0530

    updated

--- lib/view_model/splash_controller.dart
+++ lib/view_model/splash_controller.dart
@@ -1,13 +1,14 @@
 
 import 'package:community/view/ui/login_screen.dart';
 import 'package:get/get.dart';
+import 'package:community/utils/constants.dart';
 
 
 class SplashController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: splashScreenDelay), () {
       Get.offAll(() => LoginScreen());
     });
   }
