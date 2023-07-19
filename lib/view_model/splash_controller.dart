2f0fd2665ea5a39b658f95a16c83be0e41916a39
Abiidahiya <dahiya.abhi@live.com>
2023-07-20 03:23:50 +0530

    method to check if user already logged in or not implemented

--- lib/view_model/splash_controller.dart
+++ lib/view_model/splash_controller.dart
@@ -5,6 +5,8 @@ import 'package:community/utils/constants.dart';
 import 'package:community/utils/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:community/view/ui/home_page.dart';
+import 'package:community/utils/constants.dart';
+import 'package:community/view/ui/login_screen.dart';
 
 
 
@@ -12,8 +14,21 @@ class SplashController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    Future.delayed(const Duration(seconds: splashScreenDelay), () {
-      Get.offAll(() => LoginScreen());
-    });
+    checkAuthentication();
+  }
+
+  Future<void> checkAuthentication() async {
+    await Future.delayed(const Duration(seconds: splashScreenDelay)); // Simulate a loading delay
+
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final User? user = auth.currentUser;
+
+    if (user != null) {
+      // User is already logged in, navigate to the homepage
+      Get.offAll(() => const HomePage());
+    } else {
+      // User is not logged in, navigate to the sign-in page
+      Get.offAll(() => const LoginScreen());
+    }
   }
 }
